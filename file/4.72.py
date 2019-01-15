2f1e74df969045682d3123c04a5d08e58a7e538f
Hisham Anver <hisham.anver@team.telstra.com>
2019-01-16 10:20:19 +1100

    new folder for 4.72

--- file/4.72.py
+++ file/4.72.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 
-with open("file1.txt") as file1:
+with open("4.72\\file1.txt") as file1:
     data = file1.read()
-    with open("file2.txt") as file2:
+    with open("4.72\\file2.txt") as file2:
         data = data + "\n" + file2.read()
-        with open("file3.txt") as file3:
+        with open("4.72\\file3.txt") as file3:
             data = data + "\n" + file3.read()
-            with open(datetime.now().strftime("%Y-%m-%d-%H-%M-%S-%f.txt"), "w") as fileOut:
+            with open(datetime.now().strftime("4.72\\%Y-%m-%d-%H-%M-%S-%f.txt"), "w") as fileOut:
                 fileOut.write(data)
 
 #Try glob2.glob("*.txt")
